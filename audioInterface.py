75a6409211edbc366a77ab27131560e2bd8898f1
NGH <nick.hayeck@gmail.com>
2018-07-22 22:04:26 -0400

    made it OOP

--- audioInterface.py
+++ audioInterface.py
@@ -2,6 +2,15 @@ import sys
 import serial
 import tsunamiSerial
 
+
+    ##################################################
+    # Serial Port to be used. Look in device manager #
+    # for COM port name. Should look like "COM4"     #
+    ##################################################
+
+interfacePort = 'COM4'
+
+
 ##################################################
 ################ Begin Parser ####################
 ##################################################
@@ -11,9 +20,9 @@ import tsunamiSerial
 if len(sys.argv)  == 1:
     sys.exit('Too few arguments')
 
-if tsunamiSerial.serialPort == '':
+if interfacePort == '':
     print("\n\n+---------------------------------------------+")
-    print("+ PLEASE SET SERIAL PORT IN tsunamiSerial.py +")
+    print("| PLEASE SET SERIAL PORT IN audioInterface.py |")
     print("+---------------------------------------------+\n")
 
     sys.exit(1)
@@ -26,22 +35,25 @@ if '-h' in sys.argv or '--help' in sys.argv:
           "\n\nCommands:\n info\n play\n pause\n stop\n resume\n track_volume\n output_volume\n loop\n fade\n sample_offset\n load\n"+
           "\n\n\nRefer to documentation for more info on each command")
     sys.exit(0)
-#for logging purposes; port does not actually open here, but rather is opened during the import statement. A terrible way to do it... I know.    
-print('Opening port on \"{}\"...\n'.format(serialPort))
+
+print('Opening port on \"{}\"...\n'.format(interfacePort))
+
+serialControl = tsunamiSerial.TsunamiSerial(interfacePort)
+
 
 #info command
 if sys.argv[1] == 'info':
-    print(tsunamiSerial.getSysInfo())
+    print(serialControl.getSysInfo())
 
 
 #Stop command
 if sys.argv[1] == 'stop':
     if len(sys.argv) == 2:
-        tsunamiSerial.stop_all()
+        serialControl.stop_all()
     elif len(sys.argv) != 4:
         sys.exit('Too many or too few arguments for the STOP command. If you\'re trying to stop a \nspecific track, it should be of the form: stop [track] [output]')
     else:
-        tsunamiSerial.controlTrack(sys.argv[2], 'stop', sys.argv[3])
+        serialControl.controlTrack(sys.argv[2], 'stop', sys.argv[3])
         print("Stop command for track {} sent".format(sys.argv[2]))
 
 
@@ -52,7 +64,7 @@ if sys.argv[1] == 'resume':
     elif len(sys.argv) != 4:
         sys.exit('Too many or too few arguments for the RESUME command. If you\'re trying to resume a \nspecific track, it should be of the form: resume [track] [output]')
     else:
-        tsunamiSerial.controlTrack(sys.argv[2], 'resume', sys.argv[3])
+        serialControl.controlTrack(sys.argv[2], 'resume', sys.argv[3])
         print("Resume command for track {} sent".format(sys.argv[2]))
 
 
@@ -61,7 +73,7 @@ if sys.argv[1] == 'output_volume':
     if len(sys.argv) != 4:
         sys.exit('Too many or too few arguments for the output_volume command. It should be of the form: output_volume [output] [gain]')
     else:
-        tsunamiSerial.outputVolume(sys.argv[2], sys.argv[3])
+        serialControl.outputVolume(sys.argv[2], sys.argv[3])
         print("Volume command for output {} sent".format(sys.argv[2]))
 
 #track_volume command
@@ -69,7 +81,7 @@ if sys.argv[1] == 'track_volume':
     if len(sys.argv) != 4:
         sys.exit('Too many or too few arguments for the track_volume command. It should be of the form: track_volume [track] [gain]')
     else:
-        tsunamiSerial.trackVolume(sys.argv[2], sys.argv[3])
+        serialControl.trackVolume(sys.argv[2], sys.argv[3])
         print("Volume command for track {} sent".format(sys.argv[2]))
 
 
@@ -78,7 +90,7 @@ if sys.argv[1] == 'sample_offset':
     if len(sys.argv) != 4:
         sys.exit('Too many or too few arguments for the sample_offset command. It should be of the form: sample_offset [output] [offset]')
     else:
-        tsunamiSerial.sampleRateOffset(sys.argv[2], sys.argv[3])
+        serialControl.sampleRateOffset(sys.argv[2], sys.argv[3])
         print("Offset command for output {} sent".format(sys.argv[2]))
 
 
@@ -87,7 +99,7 @@ if sys.argv[1] == 'play':
     if len(sys.argv) != 4:
         sys.exit('Too many or too few arguments for the PLAY command. It should be of the form: play [track] [output]')
     else:
-        tsunamiSerial.controlTrack(int(sys.argv[2]), 'play', int(sys.argv[3]))
+        serialControl.controlTrack(int(sys.argv[2]), 'play', int(sys.argv[3]))
         print("Play command for track {} sent".format(sys.argv[2]))
 
 #Pause command
@@ -95,7 +107,7 @@ if sys.argv[1] == 'pause':
     if len(sys.argv) != 4:
         sys.exit('Too many or too few arguments for the PAUSE command. It should be of the form: pause [track] [output]')
     else:
-        tsunamiSerial.controlTrack(sys.argv[2], 'pause', sys.argv[3])
+        serialControl.controlTrack(sys.argv[2], 'pause', sys.argv[3])
         print("Pause command for track {} sent".format(sys.argv[2]))
 
 #Load command
@@ -103,7 +115,7 @@ if sys.argv[1] == 'load':
     if len(sys.argv) != 4:
         sys.exit('Too many or too few arguments for the LOAD command. It should be of the form: load [track] [output]')
     else:
-        tsunamiSerial.controlTrack(sys.argv[2], 'load', sys.argv[3])
+        serialControl.controlTrack(sys.argv[2], 'load', sys.argv[3])
         print("Load command for track {} onto output {} sent".format(sys.argv[2], sys.argv[3]))
 
 
@@ -112,7 +124,7 @@ if sys.argv[1] == 'fade':
     if len(sys.argv) != 6:
         sys.exit('Too many or too few arguments for the FADE command. It should be of the form: fade [track] [gain] [time] [stop]')
     else:
-        tsunamiSerial.trackFade(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+        serialControl.trackFade(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
         print("Fading track {} {}dB over {} sent".format(sys.argv[2], sys.argv[3], sys.argv[4]))
 #Loop command
 if sys.argv[1] == 'loop':
@@ -121,7 +133,9 @@ if sys.argv[1] == 'loop':
     if sys.argv[5] not in ['on', 'off']:
         sys.exit('Third argument for command LOOP should either be on or off')
     else:
-        tsunamiSerial.controlTrack(sys.argv[2], ('loop on' if sys.argv[5] == 'on' else 'loop off'), sys.argv[3])
+        serialControl.controlTrack(sys.argv[2], ('loop on' if sys.argv[5] == 'on' else 'loop off'), sys.argv[3])
         print("Loop command for track {} onto output {} sent".format(sys.argv[2], sys.argv[3]))
-tsunamiSerial.s.close()
-print("\nClosing connection on {}...".format(tsunamiSerial.serialPort))
+
+
+serialControl.closePort()
+print("\nClosing connection on {}...".format(interfacePort))
